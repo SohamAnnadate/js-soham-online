2f87ace8d9db861d78d6d18376d889dfb2911170
Soham Santosh Annadate <sohamannadate007@gmail.com>
2025-08-07 18:56:00 +0000

    changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const  accountId = 152451
+let accountEmail = "soham@google.com"
+var accountPassword = 12345
+accountCity = "Jaipur"
+
+// accountId = 2 // Not Allowed
+
+accountEmail = "sohamannadate1718@gmail.com"
+accountPassword = 552233
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
