634adf11fcf4795d854965c7bc2d442550e12816
Soham Santosh Annadate <sohamannadate007@gmail.com>
2025-08-07 19:06:22 +0000

    let and consr done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,11 @@ const  accountId = 152451
 let accountEmail = "soham@google.com"
 var accountPassword = 12345
 accountCity = "Jaipur"
+let accountState;
+
+/*
+    If we only decalre a variable without any value then javascript by default assumes it to 'UNDEFINED'
+*/
 
 // accountId = 2 // Not Allowed
 
@@ -11,4 +16,14 @@ accountCity = "Pune"
 
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+
+In javascript programming use only 
+    const and let 
+datatypes 
+    avoide the var 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
