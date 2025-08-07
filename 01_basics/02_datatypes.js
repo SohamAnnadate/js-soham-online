80cdbdff5a52644f9e49ff17e005351be83a5809
Soham Santosh Annadate <sohamannadate007@gmail.com>
2025-08-07 19:30:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treats all the JS code as newer js version
+
+// alert("hey i am a coder..!")  // we are using nodejs, not browser
+
+console.log(3 
+    + 3); // code readibility should be high // incorrect
+
+console.log("Soham") // correct 
+
+let name = "Soham"  //String datatype
+let roll = 4        //Integer datatype
+let isLoggedIn = true   //Boolean datatype
+
+/*
+Datatypes :
+    number => 2 to the power 53 (range)
+    bigint
+    string => "" (prefer "" quotes)
+    boolean => true/false,  0/1
+    null => standalone value
+    undefined => 
+    symbol => mostly used for uniqueness
+
+
+    object
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);     // object
+
